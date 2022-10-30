ceeb9bd7fd0937d5d84d8b3c75b8f34432ce6a3f
drillfreak100 <blusky_8893@hotmail.com>
2022-10-31 00:01:03 +0800

    chore(eslint): include semi

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -3,10 +3,6 @@ module.exports = {
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve('next/babel')],
-    },
+    semi: 'error',
   },
 };
