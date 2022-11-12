264e3f7356c6066adbecfd3562a12e90288890e6
Ng Charn Chuen <blusky_8893@hotmail.com>
2022-11-12 05:43:06 +0000

    chore(eslint-config-custom): include typescript eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,12 @@
 module.exports = {
   ignorePatterns: ['dist/*', 'node_modules/*'],
   extends: ['next', 'next/core-web-vitals', 'turbo', 'prettier'],
+  plugins: ['@typescript-eslint'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     '@next/next/no-img-element': 0,
     'react/jsx-key': 'off',
     semi: 'error',
-    'no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
 };
